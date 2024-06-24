cb3a70be5347732484e6f3f0337b6e2f6467023b
grossiwm <grossiwm@gmail.com>
2024-06-24 00:16:27 -0300

    fix imports and traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
 pub mod api {
-    use chat_p2p::{ChatP2P, Message};
+    use p2p::chat_p2p::ChatP2P;
+    use chat::message::Message;
+    use std::error::Error;
 
     pub struct ChatAPI {
         chat_session: ChatP2P,
     }
 
     impl ChatAPI {
-        pub async fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        pub async fn new() -> Result<ChatAPI, Box<dyn Error>> {
             let chat_session = ChatP2P::new().await?;
-            Ok(Self { chat_session })
+            Ok(ChatAPI { chat_session })
         }
 
-        pub async fn create_offer(&self) -> Result<String, Box<dyn std::error::Error>> {
-            self.chat_session.create_offer().await
+        pub async fn create_offer(&self) -> Result<String, Box<dyn Error>> {
+            let offer = self.chat_session.create_offer().await?;
+            Ok(offer)
         }
 
-        pub async fn set_remote_description(&self, sdp: &str) -> Result<(), Box<dyn std::error::Error>> {
-            self.chat_session.set_remote_description(sdp).await
+        pub async fn set_remote_description(&self, sdp: &str) -> Result<(), Box<dyn Error>> {
+            self.chat_session.set_remote_description(sdp).await?;
+            Ok(())
         }
 
         pub fn send_message(&self, message: Message) {
